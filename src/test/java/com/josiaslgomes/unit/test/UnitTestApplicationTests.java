48c2e03acfb843a0e4c5b3d80ccf77d9be536b12
Josias <josias@icomp.ufam.edu.br>
2021-02-11 12:25:47 -0400

    Commit inicial do projeto

---
+++ src/test/java/com/josiaslgomes/unit/test/UnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josiaslgomes.unit.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
